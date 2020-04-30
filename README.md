1d57b3194b6191f0c51439f2108f3491c1c7b503
Wowbcf <brendenferguson@gmail.com>
2020-04-29 20:43:50 -0700

    formatting

--- README.md
+++ README.md
@@ -8,4 +8,56 @@
 
 2. Run the script with `node imdbGeneral.js`
 
-3. Results will be a text file for each season in JSON format.
+3. Results will be a text file for each season in JSON format:
+
+[{
+    "episode": 1,
+    "title": "Winter Is Coming",
+    "rating": 9.1,
+    "votes": 38632
+},{
+    "episode": 2,
+    "title": "The Kingsroad",
+    "rating": 8.8,
+    "votes": 29279
+},{
+    "episode": 3,
+    "title": "Lord Snow",
+    "rating": 8.7,
+    "votes": 27691
+},{
+    "episode": 4,
+    "title": "Cripples, Bastards, and Broken Things",
+    "rating": 8.8,
+    "votes": 26282
+},{
+    "episode": 5,
+    "title": "The Wolf and the Lion",
+    "rating": 9.1,
+    "votes": 27346
+},{
+    "episode": 6,
+    "title": "A Golden Crown",
+    "rating": 9.2,
+    "votes": 27076
+},{
+    "episode": 7,
+    "title": "You Win or You Die",
+    "rating": 9.2,
+    "votes": 27551
+},{
+    "episode": 8,
+    "title": "The Pointy End",
+    "rating": 9.0,
+    "votes": 25640
+},{
+    "episode": 9,
+    "title": "Baelor",
+    "rating": 9.6,
+    "votes": 36209
+},{
+    "episode": 10,
+    "title": "Fire and Blood",
+    "rating": 9.5,
+    "votes": 31782
+}]
--- imdbGeneral.js
+++ imdbGeneral.js
@@ -1,97 +1,114 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //edit these, title comes from episode list pages eg https://www.imdb.com/title/tt0944947/episodes?season=1
-    const titleID = 'tt0944947'
-    const numSeasons = 8
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-    console.log('Script is now running, kick back and relax for a few minutes.')
-    for (i=1;i<=numSeasons;i++){
-        await page.goto(`https://www.imdb.com/title/${titleID}/episodes?season=${i}`,{waitUntil: 'networkidle2'});
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-        let data = await page.evaluate(()=>{
-            let x = Array.from(document.querySelectorAll('#episodes_content > div.clear > div.list.detail.eplist > [class*="list_item"]'))
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //edit these, title comes from episode list pages eg https://www.imdb.com/title/tt0944947/episodes?season=1
+  const titleID = "tt0944947";
+  const numSeasons = 8;
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+  console.log("Script is now running, kick back and relax for a few minutes.");
+  for (i = 1; i <= numSeasons; i++) {
+    await page.goto(
+      `https://www.imdb.com/title/${titleID}/episodes?season=${i}`,
+      { waitUntil: "networkidle2" }
+    );
 
-            // episode number
-            let episode = []
-            for (ep = 1;ep<=x.length;ep++){
-                episode.push(ep)
-            }
+    let data = await page.evaluate(() => {
+      class EpisodeData {
+        constructor(episode, title, rating, votes) {
+          this.episode = episode;
+          this.title = title;
+          this.rating = rating;
+          this.votes = votes;
+        }
+      }
+      let x = Array.from(
+        document.querySelectorAll(
+          '#episodes_content > div.clear > div.list.detail.eplist > [class*="list_item"]'
+        )
+      );
 
-            // title
-            let tmp = Array.from(document.querySelectorAll('#episodes_content > div.clear > div.list.detail.eplist > [class*="list_item"] > div.info > strong > a'))
-            let title = []
-            for (each of tmp){
-                if (title!== undefined){
-                    title.push(each.innerText)
-                }
-            }
+      // episode number
+      let episode = [];
+      for (ep = 1; ep <= x.length; ep++) {
+        episode.push(ep);
+      }
 
-            // rating
-            tmp = Array.from(document.querySelectorAll('#episodes_content > div.clear > div.list.detail.eplist > [class*="list_item"] > div.info > div.ipl-rating-widget > div.ipl-rating-star.small > span.ipl-rating-star__rating'))
-            let rating = []
-            for (each of tmp){
-                if (rating!== undefined){
-                    rating.push(each.innerText)
-                }
-            }
+      // title
+      let tmp = Array.from(
+        document.querySelectorAll(
+          '#episodes_content > div.clear > div.list.detail.eplist > [class*="list_item"] > div.info > strong > a'
+        )
+      );
+      let title = [];
+      for (each of tmp) {
+        if (title !== undefined) {
+          title.push(each.innerText);
+        }
+      }
 
-             // total votes
-             tmp = Array.from(document.querySelectorAll('#episodes_content > div.clear > div.list.detail.eplist > [class*="list_item"] > div.info > div.ipl-rating-widget > div.ipl-rating-star.small > span.ipl-rating-star__total-votes'))
-             let votes = []
-             for (each of tmp){
-                 if (votes!== undefined){
-                     votes.push(each.innerText)
-                 }
-             }
-            
-             class EpisodeData {
-                 constructor(episode,title,rating,votes){
-                    this.episode = episode
-                    this.title = title
-                    this.rating = rating
-                    this.votes = votes
-                 }
-             }
+      // rating
+      tmp = Array.from(
+        document.querySelectorAll(
+          '#episodes_content > div.clear > div.list.detail.eplist > [class*="list_item"] > div.info > div.ipl-rating-widget > div.ipl-rating-star.small > span.ipl-rating-star__rating'
+        )
+      );
+      let rating = [];
+      for (each of tmp) {
+        if (rating !== undefined) {
+          rating.push(each.innerText);
+        }
+      }
 
-            let formatted = []
-            let t
-            for (ep of episode){
-                t = new EpisodeData(episode[ep-1],title[ep-1],rating[ep-1],votes[ep-1])
-                try {
-                    votefunc = Number(t.votes.match(/\d/g).join(''))
-                } catch (error) {
-                    votefunc = 0
-                }
-                formatted.push(`
-                {
-                    "episode": ${t.episode},
-                    "title": "${t.title}",
-                    "rating": ${t.rating},
-                    "votes": ${votefunc}
-                }
-                `)
+      // total votes
+      tmp = Array.from(
+        document.querySelectorAll(
+          '#episodes_content > div.clear > div.list.detail.eplist > [class*="list_item"] > div.info > div.ipl-rating-widget > div.ipl-rating-star.small > span.ipl-rating-star__total-votes'
+        )
+      );
+      let votes = [];
+      for (each of tmp) {
+        if (votes !== undefined) {
+          votes.push(each.innerText);
+        }
+      }
 
-            }
+      let formatted = [];
+      let t;
+      for (ep of episode) {
+        t = new EpisodeData(
+          episode[ep - 1],
+          title[ep - 1],
+          rating[ep - 1],
+          votes[ep - 1]
+        );
+        try {
+          votefunc = Number(t.votes.match(/\d/g).join(""));
+        } catch (error) {
+          votefunc = 0;
+        }
+        formatted.push(
+          `{
+    "episode": ${t.episode},
+    "title": "${t.title}",
+    "rating": ${t.rating},
+    "votes": ${votefunc}
+}`
+        );
+      }
 
-            return `[${formatted}]`
-        })
-        
-        fs = require('fs')
-        fs.writeFile(`Season${i}.txt`, data, function (err) {
-            if (err) return console.log(err);
-            console.log(`Season ${i-1} complete.`);
-          });
-        
-        
-        
-    }
-  
+      return `[${formatted}]`;
+    });
 
-    await browser.close()
-  
+    fs = require("fs");
+    fs.writeFile(`Season${i}.txt`, data, function (err) {
+      if (err) return console.log(err);
+      console.log(`Season ${i - 1} complete.`);
+    });
+  }
+
+  await browser.close();
 })();
